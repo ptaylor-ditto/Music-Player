71ca85e97de73be6d7bcc7ab6f30985b78c4dbea
Parker Taylor <85856503+ptaylor-ditto@users.noreply.github.com>
2022-04-28 17:19:50 -0600

    Update musicPlayer.py

--- musicPlayer.py
+++ musicPlayer.py
@@ -1,6 +1,5 @@
 import PySimpleGUI as sg
-import pygame, os, audio_metadata
-from random import randint
+import pygame, os
 pygame.init()
 songs = ['baby-shark.mp3', 'rick_astley.mp3', 'NyanCat.mp3', 'CantinaSong.mp3']
 os.system('cls')
@@ -10,10 +9,12 @@ fullPathToMusicFile = pathSep.join([os.getcwd(), songs[x]])
 pygame.mixer.music.load(fullPathToMusicFile)
 layout = [
     [sg.Text('Volume  '), sg.Slider(range=(0,100), orientation='h', size=(60,10), key=('volume'))],
-    [sg.Text('Song Progress'), sg.Slider(range=(0,0), orientation='h', key='song')],
-        [sg.Button(image_filename='back_button.png', size=(40,40), auto_size_button=True, key='back'),
-        sg.Button(image_filename='play_button.png', size=(40,40), auto_size_button=True, key='play/pause', visible=True),
-        sg.Button(image_filename='forward_button.png', size=(40,40), auto_size_button=True, key='forward')
+    [sg.Button(image_filename='back_ten.png', size=(20,20), key='back_ten'),
+        sg.Button(image_filename='forward_ten.png', size=(20,20), key='forward_ten')
+    ],
+        [sg.Button(image_filename='back_button.png', size=(40,40), key='back'),
+        sg.Button(image_filename='play_button.png', size=(40,40), key='play/pause', visible=True),
+        sg.Button(image_filename='forward_button.png', size=(40,40), key='forward')
         ]
 ]
 window = sg.Window('Music Player', layout)
@@ -23,18 +24,19 @@ def play(x):
     music = songs[x]
     pygame.mixer.music.load(music)
     pygame.mixer.music.play()
-length = audio_metadata.load(fullPathToMusicFile)
 while True:
     event, values = window.read()
     length = pygame.mixer.music.get_pos()
-    print(length)
     pygame.mixer.music.set_volume(values['volume']/100)
-    pygame.mixer.music.set_pos(values['song'])
     if event == sg.WIN_CLOSED:
         break
+    if event == 'back_ten':
+        length = pygame.mixer.music.get_pos()
+        pygame.mixer.music.set_pos(length-10)
+    if event == 'forward_ten':
+        length = pygame.mixer.music.get_pos()
+        pygame.mixer.music.set_pos(length+10)
     if event == 'play/pause':
-        length = audio_metadata.load(songs[x])
-        window.Element('song').Update(range=(0,length))
         if pygame.mixer.music.get_busy() == True:
             pygame.mixer.music.pause()
             window.Element('play/pause').Update(image_filename='play_button.png')
